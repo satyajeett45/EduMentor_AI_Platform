b1d34f9c2e6be2346b50b16b82f80cbc27d2d930
satyajeett45 <satyajeett2718@gmail.com>
2025-06-09 12:05:41 +0530

    config file add

---
+++ config.py
@@ -0,0 +1,49 @@
+#config.py 
+import os
+
+# -------------------------------
+# Feature Configuration
+# -------------------------------
+
+NUMERICAL_FEATURES = [
+    'math_grade', 'english_grade', 'science_grade', 'history_grade', 'overall_grade',
+'assignment_completion', 'engagement_score', 'math_lec_present', 'science_lec_present',
+'history_lec_present', 'english_lec_present', 'attendance_ratio', 'login_frequency_per_week',
+'average_session_duration_minutes', 'completed_lessons', 'practice_tests_taken', 'lms_test_scores'
+
+]
+
+CATEGORICAL_FEATURES = [
+    'learning_style',
+    'content_type_preference',
+    'teacher_comments_summary'
+]
+
+TARGET_COLUMN = 'risk_score'
+CLASSIFICATION_COLUMN = 'is_at_risk'
+
+# -------------------------------
+# File Paths
+# -------------------------------
+
+MODEL_PATH = 'artifacts/best_model.pkl'
+REGRESSOR_MODEL_PATH =  'artifacts/best_regression_model.pkl'
+CLASSIFIER_MODEL_PATH = 'artifacts/best_classification_model.pkl'
+SCALER_PATH = 'artifacts/scaler.pkl'
+ENCODERS_PATH = 'artifacts/label_encoders.pkl'
+STUDENT_DATA_PATH = r'C:\Users\Admin\OneDrive\Desktop\Edumentor platform\Data\edu_mentor_dataset_final.csv'
+
+# -------------------------------
+# LLM & API Keys (Set via .env or environment)
+# -------------------------------
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+
+
+# -------------------------------
+# Thresholds & Flags
+# -------------------------------
+
+RISK_THRESHOLD = 30       # Below this = low risk
+CLASSIFICATION_THRESHOLD = 0.5   # Probability threshold for binary output
+SUGGESTIONS_ENABLED = True
